47967473d0a8faa99be5aada74f6779898c61678
yujia.han <yujia.han@qunar.com>
2016-09-13 10:59:26 +0800

    dish

--- components/dish/index.js
+++ components/dish/index.js
@@ -1,11 +1,35 @@
 /**
+ *  菜品属性
+ *     名称:     name
+ *     状态:     status
+ *     生成时间:  initTime
+ *     备注:     remark
+ *     制作时间:  timeConsume
+ *     数量 :     num
+ *     可做厨师列表:  chefList
+ *     紧急程度:   emergency
+ *     绑定订单号:  bindOrderNum
+ *     是否支持合并: canMerge
+ *     修改日志:     log
  *
+ *  菜品状态机:
+ *
+ *
+ *
+ *  影响菜品状态的action:   新单  催菜  退菜  厨师开始做某个菜
  */
 
-
-
-function  init (){
-
+var DISHSTATUS = {
+    "new": "待安排",
+    "arranged": "已排序",
+    "making": "制作中",
+    "timeOut": "超时",
+    "complete": "完成",
+    "canceled": "被取消"
 }
 
-module.exports = init;
+function  init (){
+    return "aaa";
+}
+
+exports.init = init;
