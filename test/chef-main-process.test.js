388f2381df14b6dc870f1d87dad4466d87877e11
yujia.han <yujia.han@qunar.com>
2016-10-10 11:01:39 +0800

    test

---
+++ test/chef-main-process.test.js
@@ -0,0 +1,7 @@
+require('mocha');
+var expect = require('chai').expect;
+var mainOperate = require('../mainOperate.js');
+var orderOperate = require('../components/order/index.js');
+var dishOperate = require('../components/dish/index.js');
+var chef = require('../components/chef/index.js');
+
